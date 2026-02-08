5688d43cf2ef3774c7f2075c12efce07559179c1
unknown <amir@ameer.nmc.gov>
2026-02-08 22:52:20 +0200

    Initial static healthcare website

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                about: resolve(__dirname, 'about.html'),
+                services: resolve(__dirname, 'services.html'),
+                radiology: resolve(__dirname, 'radiology.html'),
+                contact: resolve(__dirname, 'contact.html'),
+            },
+        },
+    },
+})
